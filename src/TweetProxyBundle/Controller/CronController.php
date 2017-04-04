5b33701cdca46032fddf238a490e7288cf501a15
Dario Trbovic <timmy.dario@gmail.com>
2017-04-04 17:34:51 +0200

    working on fetching tweets

--- src/TweetProxyBundle/Controller/CronController.php
+++ src/TweetProxyBundle/Controller/CronController.php
@@ -2,11 +2,23 @@
 namespace TweetProxyBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use TweetProxyBundle\Helper\Twitter;
 
 class CronController extends Controller
 {
     public function fetchTweetAction()
     {
+        $Twitter = new Twitter;
+        $connection = $Twitter->init();
+        $results = $this->getDoctrine()->getRepository('TweetProxyBundle:User')->findAll();
+        foreach($results as $result)
+        {
+            $tweets = $connection->get("statuses/user_timeline", ["screen_name" => $result->username, 'count' => 20]);
+            foreach($tweets as $tweet)
+            {
+                
+            }
+        }
 
     }
 }
